c46f8cd1a3cb0369d61b622150647059a17e60e7
Igor <Igor@Alpha.home>
2017-03-31 13:05:58 +0300

    Bishop moves added.

--- src/Chess.java
+++ src/Chess.java
@@ -13,10 +13,10 @@ public class Chess {
         {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
         {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', 'N', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', 'B', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', 'N', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', ' ', 'P', 'P', 'P', 'P', 'P', 'P'},
         {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
     };
 
@@ -93,6 +93,12 @@ public class Chess {
                 }
                 if (move.length() == 4 && move.charAt(0) == 'n') {
                     movedSuccessfully = ambiguousKnightMove(move);
+                }
+                if (move.length() == 3 && move.charAt(0) == 'b') {
+                    movedSuccessfully = unambiguousBishopMove(move);
+                }
+                if (move.length() == 4 && move.charAt(0) == 'b') {
+                    movedSuccessfully = ambiguousBishopMove(move);
                 }
             }
         }
@@ -370,7 +376,7 @@ public class Chess {
         int cn_i = -1;
         int cn_j = -1;
 
-        // specifier tells which rook to move, either rank or file
+        // specifier tells which knight to move, either rank or file
         int s_i = -1;
         int s_j = -1;
 
@@ -404,6 +410,129 @@ public class Chess {
             if (canMoveKnight && cn_i != -1 && cn_j != -1) {
                 board[cn_i][cn_j] = ' ';
                 board[i][j] = 'N';
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean unambiguousBishopMove(String move) {
+        boolean firstMove = true;
+        boolean canMove = true;
+
+        int i = 8 - Character.getNumericValue(move.charAt(2));
+        int j = (int) move.charAt(1) - 97;
+        // candidate bishop position
+        int c_i = -1;
+        int c_j = -1;
+
+        // make sure target square isn't occupied by own piece
+        if (!"PNBRQK".contains(String.valueOf(board[i][j]))) {
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    if (board[x][y] == 'B') {
+                        int b_i = x;
+                        int b_j = y;
+                        int[][] d = {{1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+
+                        // go in all four directions
+                        for (int f = 0; f < 4; f++) {
+                            int path_i = x + d[f][0];
+                            int path_j = y + d[f][1];
+
+                            while (path_i >= 0 && path_i < 8 && path_j >= 0 && path_j < 8) {
+                                if (path_i == i && path_j == j) {
+                                    if (firstMove) {
+                                        c_i = b_i;
+                                        c_j = b_j;
+                                        firstMove = false;
+                                    }
+                                    else {
+                                        System.out.println("More than one bishop can make that move.");
+                                        canMove = false;
+                                    }
+                                }
+                                // if next square is not empty, we stop
+                                if (board[path_i][path_j] != ' ') {
+                                    break;
+                                }
+                                path_i += d[f][0];
+                                path_j += d[f][1];
+                            }
+                        }
+                    }
+                }
+            }
+            if (canMove && c_i != -1 && c_j != -1) {
+                board[c_i][c_j] = ' ';
+                board[i][j] = 'B';
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean ambiguousBishopMove(String move) {
+        boolean firstMove = true;
+        boolean canMove = true;
+
+        int i = 8 - Character.getNumericValue(move.charAt(3));
+        int j = (int) move.charAt(2) - 97;
+        // candidate bishop position
+        int c_i = -1;
+        int c_j = -1;
+
+        // specifier tells which knight to move, either rank or file
+        int s_i = -1;
+        int s_j = -1;
+
+        if ("abcdefgh".contains(String.valueOf(move.charAt(1)))) {
+            s_j = (int) move.charAt(1) - 97;
+        }
+        else {
+            s_i = 8 - Character.getNumericValue(move.charAt(1));
+        }
+
+        // make sure target square isn't occupied by own piece
+        if (!"PNBRQK".contains(String.valueOf(board[i][j]))) {
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    if (board[x][y] == 'B') {
+                        int b_i = x;
+                        int b_j = y;
+                        int[][] d = {{1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+                        // go in all four directions
+                        for (int f = 0; f < 4; f++) {
+                            int path_i = x + d[f][0];
+                            int path_j = y + d[f][1];
+
+                            while (path_i >= 0 && path_i < 8 && path_j >= 0 && path_j < 8) {
+                                if (path_i == i && path_j == j) {
+                                    if (s_i == b_i || s_j == b_j) {
+                                        if (firstMove) {
+                                            c_i = b_i;
+                                            c_j = b_j;
+                                            firstMove = false;
+                                        }
+                                        else {
+                                            canMove = false;
+                                        }
+                                    }
+                                }
+                                // if next square is not empty, we stop
+                                if (board[path_i][path_j] != ' ') {
+                                    break;
+                                }
+                                path_i += d[f][0];
+                                path_j += d[f][1];
+                            }
+                        }
+                    }
+                }
+            }
+            if (canMove && c_i != -1 && c_j != -1) {
+                board[c_i][c_j] = ' ';
+                board[i][j] = 'B';
                 return true;
             }
         }
